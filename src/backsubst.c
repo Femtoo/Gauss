a204758378de14332029735d309e8a7ca15bd82a
Femtoo <konrad1.tr@gmail.com>
2021-01-14 20:38:45 +0100

    pierwsza wersja podstawiania wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,17 +5,27 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	double roznica;
+	x->data[b->r][0]=b->data[b->r][0]/mat->data[mat->r][mat->c];	
+	for(int i=1;i<b->r;i++) {
+		roznica=b->data[(b->r)-i][0];
+		for(int j=0;j<i;j++) {
+		roznica-=mat->data[(mat->r)-i][(mat->c)-j]*x->data[(b->r)-j][0];
+		}
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+		x->data[(b->r)-i][0]=roznica/mat->data[(mat->r)-i][(mat->c)-i];
+	}
+	
+	
+	
+	
+	/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
 
-				int i;
+	/*			int i;
 				for (i =0; i < x->r; i++) {
 								x->data[i][0] = b->data[i][0];
 				}
-
+	*/
 				return 0;
 }
 
