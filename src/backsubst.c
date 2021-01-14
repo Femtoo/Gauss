ddf8c680004e9b4a28fea91e14859b559b14394f
Femtoo <konrad1.tr@gmail.com>
2021-01-14 21:10:51 +0100

    poprawiona wersja wstecznego podstawiania v3.0 dodany warunek dziel. prz. 0

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,14 +5,15 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-	double roznica;
-	x->data[b->r-1][0]=b->data[b->r-1][0]/mat->data[mat->r-1][mat->c-1];	
-	for(int i=1;i<b->r;i++) {
+	double roznica;	
+	for(int i=0;i<b->r;i++) {
 		roznica=b->data[(b->r)-i-1][0];
 		for(int j=0;j<i;j++) {
 		roznica-=mat->data[(mat->r)-i-1][(mat->c)-j-1]*(x->data[(b->r)-j-1][0]);
 		}
-
+		
+		if(mat->data[(mat->r)-i-1][(mat->c)-i-1]==0)
+			return 1;
 		x->data[(b->r)-i-1][0]=roznica/mat->data[(mat->r)-i-1][(mat->c)-i-1];
 	}
 	
